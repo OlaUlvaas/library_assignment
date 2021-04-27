1c77f8693a2a0417cc7ab217fc6ea347d0eaad4a
olaulvas <ola.ulvaas@gmail.com>
2021-04-27 15:08:59 +0200

    Create LibraryUser  and Loan Entity.

--- src/main/java/se/lexicon/library/model/Loan.java
+++ src/main/java/se/lexicon/library/model/Loan.java
@@ -1,4 +1,90 @@
 package se.lexicon.library.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Loan {
+    private long id;
+    private LibraryUser loanTaker;
+    private Book book;
+    private LocalDate loanDay;
+    private boolean terminated;
+
+    public Loan() {
+    }
+
+    public Loan(LibraryUser loanTaker, Book book, LocalDate loanDay, boolean terminated) {
+        this.loanTaker = loanTaker;
+        this.book = book;
+        this.loanDay = loanDay;
+        this.terminated = terminated;
+    }
+
+    boolean extendLoan(int days){
+        return false;
+    }
+    boolean isOverDue(){
+        return false;
+    }
+
+    BigDecimal getFine(){
+        return null;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LibraryUser getLoanTaker() {
+        return loanTaker;
+    }
+
+    public void setLoanTaker(LibraryUser loanTaker) {
+        this.loanTaker = loanTaker;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getLoanDay() {
+        return loanDay;
+    }
+
+    public boolean isTerminated() {
+        return terminated;
+    }
+
+    public void setTerminated(boolean terminated) {
+        this.terminated = terminated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return id == loan.id && terminated == loan.terminated && Objects.equals(loanTaker, loan.loanTaker) && Objects.equals(book, loan.book) && Objects.equals(loanDay, loan.loanDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, loanTaker, book, loanDay, terminated);
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "id=" + id +
+                ", loanTaker=" + loanTaker +
+                ", book=" + book +
+                ", loanDay=" + loanDay +
+                ", terminated=" + terminated +
+                '}';
+    }
 }
--- todo.md
+++ todo.md
@@ -1,1 +1,5 @@
-Change H2 testDB to test mode, create an empty library_db.json
+Change H2 testDB to test mode, create an empty library_db.
+
+Book class, LibraryUser class, Loan class.
+
+
